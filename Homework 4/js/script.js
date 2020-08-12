f028f64ff5f9a37673a3234be0c7ec4bb32d6288
Oleh <olegkp4@gmail.com>
2020-08-12 05:39:37 +0300

    add folder with done homework 4

---
+++ Homework 4/js/script.js
@@ -0,0 +1,58 @@
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const marks = [4, 5, 5, 3, 4, 5];
+
+//1
+function getPairs() {
+    let boys = [];
+    let girls = [];
+    let pairs = [];
+    for (let i = 0; i < students.length; i++) {
+        if (students[i] === 'Олександр' || students[i] === 'Ігор' || students[i] === 'Олексій') {
+            boys.push(students[i]);
+        } else {
+            girls.push(students[i]);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        pairs.push([boys[i] + ", " + girls[i]]);
+    }
+
+    return pairs;
+}
+const pairs = getPairs(students);
+console.log(pairs);
+
+//2
+function pairsWithThemes(pairs, themes) {
+    let pairsAndThemes = [];
+    for (let i = 0; i < 3; i++) {
+      pairsAndThemes.push([pairs[i] + `, ` + themes[i]]);
+    }
+    return pairsAndThemes;
+  }
+  const pairsAndThemes = pairsWithThemes(pairs, themes);
+  console.log(pairsAndThemes);
+
+  //3
+  function marksForStudents(students, marks) {
+    let studentsMarks = [];
+    for(let i =0; i < students.length; i++){
+      studentsMarks.push([students[i] + ', ' + marks[i]])
+    }
+    return studentsMarks;
+      
+  }
+  const studentsMarks = marksForStudents(students, marks);
+  console.log(studentsMarks);
+
+  //4
+  function randomMarkForProject(pairsAndThemes) {
+      let studentsWithRandomMarks = [];
+    for(let i =0; i < pairsAndThemes.length; i++){
+        studentsWithRandomMarks.push([pairsAndThemes[i] + `, ` + Math.ceil(Math.random()*5)]);
+    }
+    return studentsWithRandomMarks;
+  }
+  const allStudentsWithRandomMarks = randomMarkForProject(pairsAndThemes);
+  console.log(allStudentsWithRandomMarks);
